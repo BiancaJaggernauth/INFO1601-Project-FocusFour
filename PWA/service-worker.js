8f4705c9adbc6d6b6a13725842de8fcf58988de7
BiancaJaggernauth <biancajaggernauth@gmail.com>
2026-04-15 22:18:42 -0400

    Cleaned root folder and updated worker

--- PWA/service-worker.js
+++ PWA/service-worker.js
@@ -14,24 +14,23 @@ NOTES FOR TEAM:
 const cacheName = 'focus-four-v1';
 const assets = [
   '/',
-  '/index.html',
-  '/login.html',
-  '/register.html',
-  '/dashboard.html',
-  '/session.html',
-  '/PWA/icon-192.png',
-  '/PWA/icon-fixed-512.png',
-  '/JS/auth.js',
-  '/JS/dashboard.js',
-  '/JS/session.js',
-  '/JS/storage.js'
+  '../index.html',
+  '../login.html',
+  '../register.html',
+  '../dashboard.html',
+  '../session.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-fixed-512.png',
+  '../JS/auth.js',
+  '../JS/dashboard.js',
+  '../JS/session.js',
 ];
 
 self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open(cacheName).then(cache => {
-      console.log('Caching FocusFour assets');
-      cache.addAll(assets);
+      return cache.addAll(assets);
     })
   );
 });
